4e9964fad6bc0e9f7585755af81f3e9962a4691e
Gabriel Lima Rezende <gabriel.lima.rezende@hotmail.com>
2022-12-13 08:37:17 -0300

    testes

--- api_csharp/Models/AddContatoRequest.cs
+++ api_csharp/Models/AddContatoRequest.cs
@@ -1,11 +1,9 @@
-﻿namespace api_csharp.Models
+﻿namespace api_csharp.Models;
+public class AddContatoRequest
 {
-    public class AddContatoRequest
-    {
-        public string? Nome { get; set; }
+  public string? Nome { get; set; }
 
-        public string? Telefone { get; set; }
+  public string? Telefone { get; set; }
 
-        public string? Endereco { get; set; }
-    }
+  public string? Endereco { get; set; }
 }
